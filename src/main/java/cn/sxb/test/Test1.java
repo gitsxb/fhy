199efbf8b0e90cf7b12d41c4d50d8f8275262546
gitsxb <502406732@qq.com>
2018-07-26 14:35:21 +0800

    eeeeee

---
+++ src/main/java/cn/sxb/test/Test1.java
@@ -0,0 +1,13 @@
+package cn.sxb.test;
+
+/**
+ * Created by SXB on 2018/7/26.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println();
+        for (String arg : args) {
+            
+        }
+    }
+}
