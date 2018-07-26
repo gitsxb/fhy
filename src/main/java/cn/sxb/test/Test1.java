b581d91d1ee1de9b99f68836da2d6d2a9f6b86c2
gitsxb <502406732@qq.com>
2018-07-26 14:42:24 +0800

    eeeeee

--- src/main/java/cn/sxb/test/Test1.java
+++ src/main/java/cn/sxb/test/Test1.java
@@ -1,5 +1,7 @@
 package cn.sxb.test;
 
+import org.springframework.boot.bind.PropertySourceUtils;
+
 /**
  * Created by SXB on 2018/7/26.
  */
@@ -7,7 +9,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println();
         for (String arg : args) {
-            
+            System.out.println(222222);
         }
     }
 }
