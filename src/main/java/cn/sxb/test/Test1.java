839eea14a6dee18d12aa6e98160956f06634c70d
gitsxb <502406732@qq.com>
2018-07-26 15:02:08 +0800

    eeeeee

--- src/main/java/cn/sxb/test/Test1.java
+++ src/main/java/cn/sxb/test/Test1.java
@@ -9,8 +9,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println();
         for (String arg : args) {
-            System.out.println(222222);
-            System.out.println(11111);
+            System.out.println(222222);////////////////////
+            System.out.println(11111);////////////////
         }
         System.out.println(333333);
     }
